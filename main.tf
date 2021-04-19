fbbefedd6ee1142945c2f2b39ddf1fe2df472854
Chris Frage <chris.frage@cancom.de>
2021-04-19 16:11:28 +0200

    refactor: skipping docs link

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 # vim: set ft=terraform :
 
 # <provider_resource_name>
-# https://registry.terraform.io/providers/<source>/<version>/docs/resources/<resource_name>
-#resource "" "this" {
+#resource "skeletor" "this" {
 #}
