47a631d4a7085b82025a159449a01a1ee583e37a
Chris Frage <chris.frage@cancom.de>
2021-04-19 16:13:13 +0200

    refactor: sample output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 # vim: set ft=terraform :
+# https://www.terraform.io/docs/language/values/outputs.html
 
 # <provider_resource_name>
-#output "" {
-#  description = ""
-#  value       =
-#}
+output "skeletor_string" {
+  value       = var.skeletor_string
+  description = "Output of the skeletor string variable."
+  sensitive   = false
+}
